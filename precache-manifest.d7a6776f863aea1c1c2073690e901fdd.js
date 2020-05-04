fd2181bec1ea493c49c99419108ce8f7f9466e83
Leon van der Noll <leonvdnoll@gmail.com>
2020-05-04 12:05:47 +0200

    Updates

---
+++ precache-manifest.d7a6776f863aea1c1c2073690e901fdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c778a03368896570756aa318273ba15a",
+    "url": "/emoji-translator/index.html"
+  },
+  {
+    "revision": "d16e480b4c6ebd813373",
+    "url": "/emoji-translator/static/css/main.9454e76f.chunk.css"
+  },
+  {
+    "revision": "d7c9a715f644193e37c2",
+    "url": "/emoji-translator/static/js/2.0ce9659b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/emoji-translator/static/js/2.0ce9659b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d16e480b4c6ebd813373",
+    "url": "/emoji-translator/static/js/main.72ab4981.chunk.js"
+  },
+  {
+    "revision": "156de891046da03aad3f",
+    "url": "/emoji-translator/static/js/runtime-main.25f70641.js"
+  }
+]);
